002a622258f86d6620d0ca6bbe8681ada8649386
Zaheer Abzal <6622108+zabzal@users.noreply.github.com>
2023-06-14 23:01:02 -0400

    update default configs

--- src/apache-spark-py-hudi/conf/spark-env.sh
+++ src/apache-spark-py-hudi/conf/spark-env.sh
@@ -1,2 +1,5 @@
 # Generic options for the daemons used in the standalone deploy mode
-SPARK_LOG_DIR=/tmp/spark/logs
+export SPARK_LOG_DIR=/tmp/spark/logs
+export SPARK_WORKER_DIR=/tmp/spark/worker
+export SPARK_WORKER_PORT=34891
+
